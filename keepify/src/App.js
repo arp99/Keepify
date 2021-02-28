eaffeeb62fc0d4aa05d85fb9c4c5120dbf4440a4
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-02-28 22:33:27 +0530

    Add state for todo itmes

--- keepify/src/App.js
+++ keepify/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Display , TodoInput , TodoItem} from './components'
+import { useState } from 'react'
 
 function App() {
+  const [todoItems , setTodoItems] = useState([])
   return (
     <div className="App">
       {/* <Display />
       <TodoInput /> */}
-      <TodoItem />
+      <TodoItem  items ={todoItems} setItems={setTodoItems}/>
     </div>
   );
 }
