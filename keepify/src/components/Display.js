dad4429e2f57510a351187f08ba2bfee4c78cdc9
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-03-01 23:43:32 +0530

    Style: Arrange todos in rows

--- keepify/src/components/Display.js
+++ keepify/src/components/Display.js
@@ -4,44 +4,52 @@ export default function Display({items , setItems , pinnedTodos , setPinnedTodos
  
     const Alltodos = () =>{
         return(
-            <div className="all_todo_container">
-                <h4>{items.length>0?'All':''}</h4>
-                {
-                    items.map(({id , title , txtValue, edited})=>{
-                        return <TodoItem 
-                                    title={title}
-                                    txtValue={txtValue}
-                                    id={id}
-                                    setItems={setItems}
-                                    items={items}
-                                    setPinnedTodos={setPinnedTodos}
-                                    pinnedTodos={pinnedTodos}
-                                    key={id+edited}
-                                />
-                    })
-                }
+            <div className="wrapper">
+                {items.length>0?<h4>All</h4>:''}
+                <div className="all_todo_container">
+                    {
+                        items.map(({id , title , txtValue, edited , color , pinned})=>{
+                            return <TodoItem 
+                                        title={title}
+                                        txtValue={txtValue}
+                                        id={id}
+                                        color={color}
+                                        pinned={pinned}
+                                        setItems={setItems}
+                                        items={items}
+                                        setPinnedTodos={setPinnedTodos}
+                                        pinnedTodos={pinnedTodos}
+                                        key={id+edited}
+                                    />
+                        })
+                    }
+                </div>
             </div>
         )
     }
 
     const PinnedTodos = () =>{
         return(
-            <div className="pinned_todo_container">
-                <h4>{pinnedTodos.length>0?'Pinned':''}</h4>
-                {
-                    pinnedTodos.map(({id , title , txtValue, edited})=>{
-                        return <TodoItem 
-                                    title={title}
-                                    txtValue={txtValue}
-                                    id={id}
-                                    setItems={setItems}
-                                    items={items}
-                                    setPinnedTodos={setPinnedTodos}
-                                    pinnedTodos={pinnedTodos}
-                                    key={id+edited}
-                                />
-                    })
-                }
+            <div className="wrapper">
+                {pinnedTodos.length>0?<h4>Pinned</h4>:''}
+                <div className="pinned_todo_container">
+                    {
+                        pinnedTodos.map(({id , title , txtValue, edited , color , pinned})=>{
+                            return <TodoItem 
+                                        title={title}
+                                        txtValue={txtValue}
+                                        id={id}
+                                        color={color}
+                                        pinned={pinned}
+                                        setItems={setItems}
+                                        items={items}
+                                        setPinnedTodos={setPinnedTodos}
+                                        pinnedTodos={pinnedTodos}
+                                        key={id+edited}
+                                    />
+                        })
+                    }
+                </div>
             </div>
         )
     }
